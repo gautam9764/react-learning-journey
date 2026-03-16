695d3bcfe9a3c93f2d03f76699735a001bb18913
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-03-16 22:52:10 +0530

    react basi Components

--- p5/react basi/src/App.jsx
+++ p5/react basi/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import './App.css'
 import Jsx from './Jsx'
+import Components from './Components'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>hello form React js</h1>
       <h1>this is jsx</h1>
       <Jsx />
+      <h1>this is Components</h1>
+      <Components />
     </>
   )
 }
