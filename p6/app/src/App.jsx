ac01a6c9e22a2efb80af81da933ff3559e266a6c
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-04-27 17:30:52 +0530

    p6 / Count app

---
+++ p6/app/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [Count , setCount] = useState(0);
+
+  function increment (){
+    setCount(Count + 1);
+  }
+
+  function decrement (){
+    setCount(Count - 1);
+  }
+
+  function restart (){
+    setCount(0);
+  }
+
+  return (
+    <>  
+      <h1>Count app</h1>
+      <p>Count {Count}</p>
+
+      <div className='btn'>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
+        <button onClick={restart}>restrt</button>
+      </div>
+    </>
+  )
+}
+
+export default App
