09e41d5734ab1996fb7f735dc4e6b392343dae84
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-02 18:30:56 +0530

    p7 / all-ract / import / word capital letter

--- p7/All-React/src/App.jsx
+++ p7/All-React/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 // import header1 from './header1'  not a wokeng header 
 import Header1 from './header1'  // yes wokaeng Header 
+import Header2 from './Header2'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <h1>header</h1>
       {/* <header1/>    */}
       <Header1/>
+
 
     </>
   )
