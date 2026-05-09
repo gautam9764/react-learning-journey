ae712596304c341dd97267cc99aba01db79fd112
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-09 21:50:16 +0530

    p10 / Components / Components

---
+++ p10/components/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Components from './Components'
+
+function App() {
+
+  return (
+    <>
+      <Components/>
+    </>
+  )
+}
+
+export default App
---
+++ p10/components/src/Components.jsx
@@ -0,0 +1,9 @@
+function Components(){
+    return(
+        <>
+            <h2>Helo React Pe React</h2>
+        </>
+    )
+}
+
+export default Components
