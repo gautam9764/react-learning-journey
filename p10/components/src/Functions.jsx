4877ba80016081fbd93173ff460b1765a4c46dd5
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-10 15:00:36 +0530

    p10 / Components / function 1

--- p10/components/src/Functions.jsx
+++ p10/components/src/Functions.jsx
@@ -1,7 +1,19 @@
+function square1(){
+    return(
+        <>
+            <div className="square"></div>
+        </>
+    )
+}
+
 function Functions(){
     return(
         <>
             <h2>Function</h2>
+
+            {
+                square1()
+            }
         </>
     )
 }
