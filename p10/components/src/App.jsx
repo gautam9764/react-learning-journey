e6a63a021687ca79d8c20ef6e2a27a3fb3df3859
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-11 12:15:49 +0530

    my class

--- p10/components/src/App.jsx
+++ p10/components/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Components from './Components'
 import Functions from './Functions'
+import Myclass from './Myclass'
 
 function App() {
 
@@ -10,6 +11,9 @@ function App() {
       <h1>vs</h1>
       <Functions/>
       <h1>difference</h1>
+
+      <h1>class</h1>
+      <Myclass/>
     </>
   )
 }
---
+++ p10/components/src/Myclass.jsx
@@ -0,0 +1,14 @@
+import React , { Component } from "react";
+
+class Myclass extends Component{
+    render(){
+        return (
+            <>
+                <h2>Hello From Class Component</h2>
+            </>
+        )
+
+    }
+}
+
+export default Myclass
