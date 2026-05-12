5a149303664864a0d1f9a8777a571a2b9061b314
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-12 21:24:21 +0530

    Regular CSS File

---
+++ p11/style/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card2 from './Card2'
+
+function App() {
+  
+
+  return (
+    <> 
+      <h1 className='red'>Regular CSS File</h1>
+      <Card2/>
+    </>
+  )
+}
+
+export default App
+
---
+++ p11/style/src/Card2.jsx
@@ -0,0 +1,10 @@
+function Card2(){
+    return(
+        <>
+            <h2 className="red">card 1 jsx</h2> 
+                  
+        </>
+    )
+}
+
+export default Card2
