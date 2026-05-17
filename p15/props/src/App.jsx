be4314171e354b9b5a8640f6a173bd4d479165a6
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-17 23:19:16 +0530

    p15 Props / Props Not Allowed Props Are Immutable

--- p15/props/src/App.jsx
+++ p15/props/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import PropsBasic from './PropsBasic'
 import PropsDestructuring from './PropsDestructuring'
+import PropsNotAllowed from './PropsNotAllowed'
 
 function App() {
 
@@ -11,6 +12,9 @@ function App() {
 
       <h1>Props Destructuring</h1>
       <PropsDestructuring name={"Gatuam"} age={18}/>
+
+      <h1>Props Not Allowed Props Are Immutable</h1>
+      <PropsNotAllowed name={"Gautam"} age={18}/>
     </>
   )
   
---
+++ p15/props/src/PropsNotAllowed.jsx
@@ -0,0 +1,12 @@
+function PropsNotAllowed({name , age}){
+    return(
+        <>
+            <div className='w-40 h-30 bg-blue-600'>
+                <h1 className='text-white'>name : {name}</h1>
+                <h2 className='text-white'>age : {age}</h2>
+            </div>
+        </>
+    )
+}
+
+export default PropsNotAllowed
