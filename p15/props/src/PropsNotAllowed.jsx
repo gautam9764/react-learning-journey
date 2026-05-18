3a06ca3dd948e54e18d47690e914eaf0f47dca03
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-18 21:22:32 +0530

    p15 Props / Props Not Allowed Props Are Immutable

--- p15/props/src/PropsNotAllowed.jsx
+++ p15/props/src/PropsNotAllowed.jsx
@@ -1,9 +1,10 @@
-function PropsNotAllowed({name , age}){
+function PropsNotAllowed(Props){
+// Props.username = Props  //not allowed
     return(
         <>
             <div className='w-40 h-30 bg-blue-600'>
-                <h1 className='text-white'>name : {name}</h1>
-                <h2 className='text-white'>age : {age}</h2>
+                <h1 className='text-white'>name : {Props.name}</h1>
+                <h2 className='text-white'>age : {Props.age}</h2>
             </div>
         </>
     )
