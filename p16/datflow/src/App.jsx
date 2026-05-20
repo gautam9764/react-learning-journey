0000ffe408c83f4dc466494f97693eacf60bf794
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-20 21:49:42 +0530

    p16 / data flow component add

---
+++ p16/datflow/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import Mains from './Mains'
+
+function App() {
+ return(
+  <>
+    <Header/>
+    <Mains/>
+    <Footer/>
+  </>
+ )
+}
+
+export default App
---
+++ p16/datflow/src/Mains.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import About from './About'
+
+const Mains = () => {
+  return (
+    <div >
+      <h1>main</h1>
+      <Hero/>
+      <About/>
+    </div>
+  )
+}
+
+export default Mains
